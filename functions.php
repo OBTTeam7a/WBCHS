0c856d3fc11c06b352441172ce7a5be87999b795
Nicholas Peters <nicholaspeters212@gmail.com>
2017-07-05 11:25:19 -0400

    add php functionaity

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+    function wbchs_resources(){
+        wp_enqueue_style('style', get_stylesheet_uri());
+    }
+    add_action('wp_enqueue_scripts', 'wbchs_resources');
+	//theme setup
+	function bannerPic(){
+    // Navigation Menus
+    register_nav_menus(array(
+        'prime'=>__('Primary Menu')
+    ));
+	// featured Image
+	add_theme_support('post-tumbnails');
+	add_image_size('small-thumbnail', 180,120, true);
+	add_image_size('banner-image',920,210, array('center', 'center'));
+}
+add_action('after_setup_theme', 'bannerPic');
+?>
